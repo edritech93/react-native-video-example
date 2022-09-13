5e1f7af364cadd15e477ea0ef8875a39594d902d
Yudi Alviska <yudi.a@weefer.co.id>
2022-09-13 16:30:36 +0700

    add react-native-video

--- App.js
+++ App.js
@@ -1,117 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
+import Video from 'react-native-video';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Self Hosted</Text>
+      <Video
+        source={{
+          uri: 'https://dev-hwtest.obs.ap-southeast-3.myhuaweicloud.com/test/docker720.mp4',
+        }}
+        ref={ref => {}} // Store reference
+        style={styles.backgroundVideo}
+        resizeMode={'contain'}
+        controls={true}
+      />
+      <Text style={styles.textTitle}>Huawei OBS</Text>
+      <Video
+        source={{
+          uri: 'http://159.138.85.77:1050/videos',
+        }}
+        ref={ref => {}} // Store reference
+        style={styles.backgroundVideo}
+        resizeMode={'contain'}
+        controls={true}
+      />
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  backgroundVideo: {
+    width: null,
+    height: 300,
+    backgroundColor: 'black',
+    marginBottom: 24,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  textTitle: {
+    fontSize: 16,
+    marginBottom: 16,
+    alignSelf: 'center',
+    color: 'black',
   },
 });
-
-export default App;
